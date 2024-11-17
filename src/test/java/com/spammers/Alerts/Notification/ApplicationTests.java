8f822d77ba8d219e35ec1da1cde48de63559c155
thesrcielos <diegomadi75@gmail.com>
2024-11-16 20:10:17 -0500

    estructura

---
+++ src/test/java/com/spammers/Alerts/Notification/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spammers.Alerts.Notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
