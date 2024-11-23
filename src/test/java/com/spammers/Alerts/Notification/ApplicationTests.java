178d815915175e28a3682d48b06d3da4cd61fa94
thesrcielos <diegomadi75@gmail.com>
2024-11-22 21:18:06 -0500

    arreglando tests

--- src/test/java/com/spammers/Alerts/Notification/ApplicationTests.java
+++ src/test/java/com/spammers/Alerts/Notification/ApplicationTests.java
@@ -1,9 +1,7 @@
 package com.spammers.Alerts.Notification;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ApplicationTests {
 
 	@Test
