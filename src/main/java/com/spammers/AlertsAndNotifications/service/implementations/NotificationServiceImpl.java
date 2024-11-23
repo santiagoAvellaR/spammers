7308d3a798ba20843638a1525d913871a30dcb77
Daniel-Aldana10 <daniel.aldana-b@mail.escuelaing.edu.co>
2024-11-22 23:44:40 -0500

    Realizando la implementacion para devolver los libros o prestamos

--- src/main/java/com/spammers/AlertsAndNotifications/service/implementations/NotificationServiceImpl.java
+++ src/main/java/com/spammers/AlertsAndNotifications/service/implementations/NotificationServiceImpl.java
@@ -5,30 +5,28 @@ import com.spammers.AlertsAndNotifications.exceptions.SpammersPublicExceptions;
 import com.spammers.AlertsAndNotifications.model.LoanDTO;
 import com.spammers.AlertsAndNotifications.model.LoanModel;
 import com.spammers.AlertsAndNotifications.model.NotificationModel;
+import com.spammers.AlertsAndNotifications.model.UserInfo;
 import com.spammers.AlertsAndNotifications.model.enums.EmailTemplate;
 import com.spammers.AlertsAndNotifications.repository.FinesRepository;
 import com.spammers.AlertsAndNotifications.repository.LoanRepository;
 import com.spammers.AlertsAndNotifications.repository.NotificationRepository;
 import com.spammers.AlertsAndNotifications.service.interfaces.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
-
+@RequiredArgsConstructor
 @Service
 public class NotificationServiceImpl implements NotificationService {
-    private FinesRepository finesRepository;
-    private LoanRepository loanRepository;
-    private EmailService emailService;
-    private NotificationRepository notificationRepository;
-
-    public NotificationServiceImpl(FinesRepository finesRepository, LoanRepository loanRepository,NotificationRepository notificationRepository, EmailService emailService) {
-        this.finesRepository = finesRepository;
-        this.loanRepository = loanRepository;
-        this.notificationRepository = notificationRepository;
-        this.emailService = emailService;
-    }
+    private final FinesRepository finesRepository;
+    private final LoanRepository loanRepository;
+    private final EmailService emailService;
+    private final NotificationRepository notificationRepository;
+    private final ApiClient apiClient;
 
     @Scheduled(cron = "0 30 8 * * ?")
     private void checkLoans(){
@@ -69,4 +67,31 @@ public class NotificationServiceImpl implements NotificationService {
     public void closeFine(String idLoan) {
 
     }
+    @Override
+    public void returnBook(LoanDTO loan, boolean returnedInBadCondition) {
+        UserInfo userInfo = apiClient.getUserInfoById(loan.getUserId());
+        LocalDate dateLoan = loan.getLoanDate();
+        DateTimeFormatter formatter  = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+        String formatteDate = dateLoan.format(formatter);
+        int days = daysDifference(loan.getLoanExpired());
+        String emailBody = buildEmailBody(formatteDate, userInfo.getGuardianEmail(), userInfo.getName(), loan.getStatus(), returnedInBadCondition, days);
+        emailService.sendEmailCustomised(userInfo.getGuardianEmail(), "Devolución de un libro", emailBody);
+    }
+    private int daysDifference(LocalDate deadline){
+        return LocalDate.now().isAfter(deadline) ? (int) ChronoUnit.DAYS.between(deadline, LocalDate.now()): 0;
+    }
+    private String buildEmailBody(String loanDate, String guardianName, String studentName, boolean statusLoan, boolean badCondition, int delay){
+        String emailbody = "Buen dia, " + guardianName + "le informamos que el dia de hoy el estudiante " + studentName + "\n" +
+                " ha hecho la devolución de un libro que tomo a préstamo el dia " + loanDate + ",\n";
+        if(!statusLoan){
+            emailbody += "sin embargo tuvo un retraso de " + delay + " dias";
+        }
+        if(badCondition){
+            emailbody += "el libro se devolvio en malas condicones.\n";
+        }
+        emailbody += "Gracias,\nCordial Saludo.\nEste es el gestor de notificaciones de Biblosoft\n";
+        emailbody += "No responder a esta cuenta de correo ya que es enviada por un motor de notificaciones automáticas.";
+        return emailbody;
+    }
+
 }
